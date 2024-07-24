a2dc847e2e5fd27d8a9fd6fd8da19feae91124a0
deepak <deepakgupta351@gmail.com>
2024-07-24 16:10:31 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+let [name,setName]=useState('');
+const displayGreetings=(event)=>{
+  setName(event.target.value);
+}
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Enter your name:</p>
+    
+    <input type="text" value={name} onChange={displayGreetings}/> 
+    {name ? <p>Hello {name}!</p> : null}
+    
+    {/* samething using and operator */}
+  {/* {name && <p>Hello {name}!</p>}   */}
+
+
+{/* gpt solution not currect */}
+    {/* {submittedName ? <h1>Hello, {submittedName}!</h1> : null} */}
     </div>
   )
 }
